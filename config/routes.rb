498d73e8fe7ff49846ca82b306cfeddf6c7a7b7e
Sharon <sharon.peishan.kennedy@gmail.com>
2017-12-06 14:34:53 -0500

    pages and auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Auth0 routes for authentication
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure'        => 'auth0#failure'
 end
