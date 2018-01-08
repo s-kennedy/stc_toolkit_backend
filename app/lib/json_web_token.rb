c022ff0a2918bb17eff55a89aaedd1573c0efc2d
Sharon <sharon.peishan.kennedy@gmail.com>
2018-01-08 11:12:16 +0100

    update env vars for production

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -6,16 +6,16 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithm: 'RS256',
-               iss: Rails.application.secrets.auth0_domain,
+               iss: ENV["AUTH0_DOMAIN"],
                verify_iss: true,
-               aud: Rails.application.secrets.auth0_api_audience,
+               aud: ENV["AUTH0_API_AUDIENCE"],
                verify_aud: true) do |header|
       jwks_hash[header['kid']]
     end
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{Rails.application.secrets.auth0_domain}.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("#{ENV["AUTH0_DOMAIN"]}.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
