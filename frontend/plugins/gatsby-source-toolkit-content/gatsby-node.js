982e003daab6a594fb52730dca549ef9e9c8348a
Sharon <sharon.peishan.kennedy@gmail.com>
2018-01-25 03:36:37 -0500

    production url for gatsby plugin

--- frontend/plugins/gatsby-source-toolkit-content/gatsby-node.js
+++ frontend/plugins/gatsby-source-toolkit-content/gatsby-node.js
@@ -3,7 +3,7 @@ const crypto = require(`crypto`);
 
 exports.sourceNodes = ({ boundActionCreators }, { resourceType }) => {
   const { createNode } = boundActionCreators;
-  const url = `http://localhost:3000/${resourceType}`;
+  const url = `https://sheltered-plains-68895.herokuapp.com/${resourceType}`;
 
   return new Promise((resolve, reject) => {
     axios.get(url)
