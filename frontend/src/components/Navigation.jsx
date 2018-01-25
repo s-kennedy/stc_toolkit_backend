c1ef9138660fb9038ee9f828c36aa672f7eebe73
Sharon <sharon.peishan.kennedy@gmail.com>
2018-01-25 13:14:16 -0500

    better nav links

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { filter } from 'lodash'
-import Link from 'gatsby-link';
+import Link, { navigateTo } from 'gatsby-link';
 import logo from '../assets/img/STC_Logo_Horiz.png';
 import AuthService from '../utils/AuthService'
 import { lock } from '../utils/init'
@@ -135,21 +135,21 @@ export default class Navigation extends React.Component {
                   Toolkit
                 </DropdownToggle>
                 <DropdownMenu>
-                  <div className='dropdown-item'>
-                    <Link className='nav-link' to='/building-blocks/analysis'>
+                  <DropdownItem onClick={() => navigateTo('/building-blocks/analysis')}>
+                    <div className='nav-link'>
                       Building Block A: Analysis
-                    </Link>
-                  </div>
-                  <div className='dropdown-item'>
-                    <Link className='nav-link' to='/building-blocks/design'>
+                    </div>
+                  </DropdownItem>
+                  <DropdownItem onClick={() => navigateTo('/building-blocks/design')}>
+                    <div className='nav-link'>
                       Building Block B: Design
-                    </Link>
-                  </div>
-                  <div className='dropdown-item'>
-                    <Link className='nav-link' to='/building-blocks/meal'>
+                    </div>
+                  </DropdownItem>
+                  <DropdownItem onClick={() => navigateTo('/building-blocks/meal')}>
+                    <div className='nav-link'>
                       Building Block C: MEAL
-                    </Link>
-                  </div>
+                    </div>
+                  </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
 
@@ -160,9 +160,9 @@ export default class Navigation extends React.Component {
                 <DropdownMenu>
                   {
                     aboutPages.map((page, index) => (
-                      <div className='dropdown-item' key={index}>
-                        <Link className='nav-link' to={`/${page.node.fields.slug}`}>{page.node.fields.title}</Link>
-                      </div>
+                      <DropdownItem key={index} onClick={() => navigateTo(`/${page.node.fields.slug}`)}>
+                        <div className='nav-link'>{page.node.fields.title}</div>
+                      </DropdownItem>
                     ))
                   }
                 </DropdownMenu>
@@ -175,9 +175,9 @@ export default class Navigation extends React.Component {
                 <DropdownMenu>
                   {
                     referencePages.map((page, index) => (
-                      <div className='dropdown-item' key={index}>
-                        <Link className='nav-link' to={`/${page.node.fields.slug}`}>{page.node.fields.title}</Link>
-                      </div>
+                      <DropdownItem key={index} onClick={() => navigateTo(`/${page.node.fields.slug}`)}>
+                        <div className='nav-link'>{page.node.fields.title}</div>
+                      </DropdownItem>
                     ))
                   }
                 </DropdownMenu>
