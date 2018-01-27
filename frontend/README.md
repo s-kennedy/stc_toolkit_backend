7cba3cf7dd9a90532d68e4a5f8edd63cfc6e94b8
Sharon <sharon.peishan.kennedy@gmail.com>
2018-01-27 17:17:20 -0500

    removed gatsby frontend

--- frontend/README.md
+++
@@ -1,5 +0,0 @@
-# Child Sensitivity in Poverty Alleviation Programming Online Toolkit
-
-An interactive toolkit created by Save the Children International
-
-
--- frontend/src/utils/init.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-import logo from '../assets/img/STC_Logo_Horiz.png'
-
-export const api = axios.create({
-                    baseURL: process.env.API_URL
-                  })
