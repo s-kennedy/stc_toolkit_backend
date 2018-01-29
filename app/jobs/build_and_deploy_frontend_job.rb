b40cdc2412beb07cb53763f626349f98a7fa2c99
Sharon <sharon.peishan.kennedy@gmail.com>
2018-01-29 12:55:07 -0500

    update deployment job

--- app/jobs/build_and_deploy_frontend_job.rb
+++ app/jobs/build_and_deploy_frontend_job.rb
@@ -2,7 +2,14 @@ class BuildAndDeployFrontendJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.logger.info "========================= Building and deploying frontend for production ==================="
-    system "cd frontend && yarn deploy"
+    if Rails.env.production?
+      Rails.logger.info "========================= Building and deploying frontend for production ==================="
+      system "cd ~/stc_toolkit_cms/frontend && yarn deploy"
+    end
+
+    if Rails.env.development?
+      Rails.logger.info "Restarting gatsby develop locally"
+      system "kill $(lsof -t -i:8000) && cd ~/stc_toolkit_cms/frontend && yarn develop &"
+    end
   end
 end
